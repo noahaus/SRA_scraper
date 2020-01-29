cf7f04339a5bdbc5ff882e8e866356193c5a54e9
Noah Legall <noahaus@uga.edu>
2020-01-29 15:17:16 -0500

    update arguments

--- SRA_scraper.r
+++ SRA_scraper.r
@@ -1,8 +1,45 @@
-install.packages("RSelenium")
-library("RSelenium")
+#check for dependencies
+if(!require("RSelenium")){
+  install.packages("RSelenium", repos = "http://cran.us.r-project.org")
+  library("RSelenium")} else {
+    library("RSelenium")  
+  }
+if(!require("optparse")){
+  install.packages("optparse", repos = "http://cran.us.r-project.org")
+  library("optparse")} else {
+    library("optparse")
+  }
+
+#Some house keeping stuff. Basically just check if there is a SRARunTable.txt in the Download folder
+wd <- getwd()
+file_name <- "SraRunTable.txt"
+
+if (Sys.info()[["sysname"]]=="Linux") {
+  default_dir <- file.path("home", Sys.info()[["user"]], "Downloads")
+} else {
+  default_dir <- file.path("", "Users", Sys.info()[["user"]], "Downloads")
+}
+
+if(file.exists(file.path(default_dir, file_name))){
+  file.remove(file.path(default_dir, file_name))
+} 
+
+#what name do you want the metadata file to be? Start with a default name
+out_name <- gsub(":","-",paste0("output","_",Sys.Date(),"_",format(Sys.time(), "%X"),".csv"))
 
 wd <- getwd()
 file_name <- "SraRunTable.txt"
+
+option_list = list(
+  make_option(c("-s", "--search"), type="character", default=NULL, help="search term used in SRA", metavar="character"),
+  make_option(c("-d", "--driver"), type="character", default="chrome", help="the type of webdriver to initialize", metavar="character"),
+  make_option(c("-o", "--output"), type="character", default= out_name, help="name of the output file", metavar="character")
+)
+
+opt_parser = OptionParser(option_list=option_list)
+opt = parse_args(opt_parser)
+
+out_name <- opt$output
 
 #open up the browser (will try to make this silent later)
 driver <- rsDriver(port = 4L, browser = "chrome", chromever = "78.0.3904.105")
@@ -11,7 +48,7 @@ remote_driver <- driver[["client"]]
 #navigate to SRA
 remote_driver$navigate("https://www.ncbi.nlm.nih.gov/sra")
 address_element <- remote_driver$findElement(using = 'name', value = 'term')
-address_element$sendKeysToElement(list('Mycobacterium bovis'))
+address_element$sendKeysToElement(list(opt$search))
 button_element <- remote_driver$findElement(using = 'id', value = "search")
 button_element$clickElement()
 
@@ -30,12 +67,5 @@ metadata$clickElement()
 Sys.sleep(3)
 driver[["server"]]$stop()
 
-
-if (Sys.info()[["sysname"]]=="Linux") {
-  default_dir <- file.path("home", Sys.info()[["user"]], "Downloads")
-} else {
-  default_dir <- file.path("", "Users", Sys.info()[["user"]], "Downloads")
-}
-
 # move the file to the desired directory
-file.rename(file.path(default_dir, file_name), file.path(wd, file_name))
+file.rename(file.path(default_dir, file_name), file.path(wd, out_name))
